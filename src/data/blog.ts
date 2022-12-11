732b8031e3474b55fd65072ae90ce37e925def10
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-11 14:36:02 +0100

    Add data and typescript type for Blog component

---
+++ src/data/blog.ts
@@ -0,0 +1,39 @@
+import { Blog } from "../types/Blog";
+
+export const blogData: Blog[] = [
+  {
+    id: 1,
+    title: "How to create a curtain menu ?",
+    date: 1,
+    tags: ["#javascript", "#css"],
+    background: "bg-gradient-to-r from-indigo-700 to-purple-800",
+  },
+  {
+    id: 2,
+    title: "How to use the .map method ?",
+    date: 1,
+    tags: ["#javascript"],
+    background: "bg-gradient-to-r from-cyan-600 to-blue-600",
+  },
+  {
+    id: 3,
+    title: "10 Flexbox tricks",
+    date: 1,
+    tags: ["#css"],
+    background: "bg-gradient-to-r from-purple-600 to-indigo-700",
+  },
+  {
+    id: 4,
+    title: "What a React props ?",
+    date: 1,
+    tags: ["#react"],
+    background: "bg-gradient-to-r from-blue-600 to-cyan-500",
+  },
+  {
+    id: 5,
+    title: "What is CSS Grid ?",
+    date: 1,
+    tags: ["#css"],
+    background: "bg-blue-600",
+  },
+];
---
+++ src/types/Blog.ts
@@ -0,0 +1,7 @@
+export type Blog = {
+  id?: number;
+  title: string;
+  tags: string[];
+  date: number;
+  background: string;
+};
