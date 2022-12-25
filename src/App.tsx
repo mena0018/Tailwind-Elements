8350ed7e18f6faaf5a466fc38664aa84ba9a4459
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-25 18:11:55 +0100

    Add components to the homepage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import BlogList from "./components/Blog/BlogList";
+import Card from "./components/Card/Card";
+import DataGrid from "./components/DataGrid/DataGrid";
+
+function App() {
+  return (
+    <>
+      <BlogList />
+      <Card />
+      <DataGrid />
+    </>
+  );
+}
+
+export default App;
